59c3f44748cf4398d5326b91ae0285432e8adc8c
pdamianik <39028343+pdamianik@users.noreply.github.com>
2020-07-24 20:51:17 +0200

    Here we go

--- dist/index.js
+++ dist/index.js
@@ -2087,11 +2087,17 @@ const github = __webpack_require__(181);
 
 try {
 	const tag = core.getInput("tag");
-	let release = github.request('GET /repos/{owner}/{repo}/releases/tags/{tag}', {
-		owner: github.context.repo.owner,
-		repo: github.context.repo.repo,
-		tag: tag
-	});
+	const token = core.getInput("token");
+
+	const oktokit = github.getOctokit(token);
+
+	let release;
+	oktokit.repos.getReleaseByTag(tag).then((value) => release = value);
+	// let release = github.request('GET /repos/{owner}/{repo}/releases/tags/{tag}', {
+	// 	owner: github.context.repo.owner,
+	// 	repo: github.context.repo.repo,
+	// 	tag: tag
+	// });
 	core.setOutput('uploadUrl', release);
 } catch (error) {
 	core.setFailed(error.message);
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ const github = require("@actions/github");
 
 try {
 	const tag = core.getInput("tag");
-	let release = github.request('GET /repos/{owner}/{repo}/releases/tags/{tag}', {
-		owner: github.context.repo.owner,
-		repo: github.context.repo.repo,
-		tag: tag
-	});
+	const token = core.getInput("token");
+
+	const oktokit = github.getOctokit(token);
+
+	let release;
+	oktokit.repos.getReleaseByTag(tag).then((value) => release = value);
+	// let release = github.request('GET /repos/{owner}/{repo}/releases/tags/{tag}', {
+	// 	owner: github.context.repo.owner,
+	// 	repo: github.context.repo.repo,
+	// 	tag: tag
+	// });
 	core.setOutput('uploadUrl', release);
 } catch (error) {
 	core.setFailed(error.message);
